e84406a4c00202391db826d66f05d90cbe68ccfd
BroVic <victorordu@outlook.com>
2016-06-30 11:17:29 +0100

    Updates .gitignore

--- directory-main.cpp
+++ directory-main.cpp
@@ -2,26 +2,37 @@
 using namespace std;
 
 class Person{
-public:
-    string firstName, surname, title;
+protected:
+string firstName, surname, title;
     long long int mobileNum;
-	Person() {
+public:
+	Person() {                   // default constructor
 	firstName = "Victor";
 	surname = "Ordu";
 	title = "Special Assistant";
 	mobileNum = 2348034510441;
 	return;
 	}
+	Person(string fn, string sn, string tt) {
+	firstName = fn; surname = sn; title = tt;
+	return;
+	}
+
 	~Person() {return;}
 };
 
 class Office:public Person {
+protected:
     int roomNum, intercomNum;
 public:
 	Office() {
 		roomNum = 203;
 		intercomNum = 161;
 		return;
+	}
+	Office(int rm, int ic) {
+	roomNum = rm; intercomNum = ic;
+	return;
 	}
 	~Office(){return;};
 	void printNum() {
@@ -30,6 +41,7 @@ public:
 	}
 };
 class Department:public Office{
+protected:
 	string dept;
 	int floorNum;
 public:
@@ -37,6 +49,10 @@ public:
 		dept = "DG's Office";
 		floorNum = 2;
 		return;
+	}
+	Department(string de, int flo) {
+	dept = de; floorNum = flo;
+	return;
 	}
 	~Department(){return;}
 };

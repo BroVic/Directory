e0401ce204a725d079c6d4d64c711eb93d6be9d6
BroVic <victorordu@outlook.com>
2016-07-10 20:19:19 +0100

    Changes objects and add new functions
    I decided to completely changed the class configuration. There really was
    no need for the derived classes and I merged those into one class. I made
    the fileOps a friend class and also made the printout more aesthetically
    appealing.

--- directory-main.cpp
+++ directory-main.cpp
@@ -3,72 +3,37 @@
 #include<cstdio>
 using namespace std;
 
-class Person{
-protected:
-string firstName, surname, title;
-    long long int mobileNum;
-public:
-	Person() {                   // default constructor
-	firstName = "Victor";
-	surname = "Ordu";
-	title = "Special Assistant";
-	mobileNum = 2348034510441;
-	return;
-	}
-	Person(string fn, string sn, string tt) {
-	firstName = fn; surname = sn; title = tt;
-	return;
-	}
+/* Objects to be created:
+1. File operations - fileOps
+2. Representation of each record on file
 
-	~Person() {return;}
-};
-class Office:public Person {
-protected:
-    int roomNum, intercomNum;
-public:
-	Office() {
-		roomNum = 203;
-		intercomNum = 161;
-		return;
-	}
-	Office(int rm, int ic) {
-	roomNum = rm; intercomNum = ic;
+*/
+
+class Record{
+	friend class fileOps;
+	private:
+		char title[30] = { 'D', 'G', '/', 'C', 'E', 'O', '\0' };
+		char intercomNum[4] = {'1', '5', '0', '\0'};
+		char dept[50] = {'D', 'G', '/', 'C', 'E', 'O', '\'', 's', 'O', 'f', 'f', 'i', 'c', 'e', '\0' };
+	public:
+	// Constructors/deconstructor
+	Record(){
 	return;
 	}
-	~Office(){return;};
-	void printNum() {
-		cout << firstName  << " " << surname << "'s intercom No. is " << intercomNum << endl;
-		return;
-	}
+	~Record(){return;}
 };
-class Department:public Office{
-protected:
-	string dept;
-	int floorNum;
-public:
-	Department(){
-		dept = "DG's Office";
-		floorNum = 2;
-		return;
-	}
-	Department(string de, int flo) {
-	dept = de; floorNum = flo;
-	return;
-	}
-	~Department(){return;}
-};
+
 class fileOps{
 	FILE *fptrIn;
 	char stringIn[100];
-	char title[30], dept[50], intcom[4];
 	char b;
-	int i, k, N;
+	int i, j, N;
 public:
 	// constructors
 	fileOps() {
 		N = 1;
 		b = ' ';
-		fptrIn = fopen("office-contact.txt", "r"); // review this line!!!
+		fptrIn = fopen("office-contact.csv", "r"); // review this line!!!
 		return;
 	}
 	~fileOps(){
@@ -79,43 +44,51 @@ public:
 	if (fptrIn == NULL){
             cout << "Could not open database" << endl;
             return true;
-    }
+            }
     else {return false;}
 	}
-	void printAll(){
-		fgets(stringIn, 99, fptrIn);
+	// Define a function for collecting record strings
+	void collectStrings(){
+	fgets(stringIn, 99, fptrIn);
+	return;
+	}
+	// Function to print all available records to the console
+	void printAll(Record &r){
+		collectStrings();
 		while(!feof(fptrIn)) {
-			i = 0; k = 0;
-			while ((title[i++] = stringIn[k++]) != ',');
-			title[i - 1] = '\0'; i = 0;
-			while ((intcom[i++] = stringIn[k++]) != ',');
-			intcom[i - 1] = '\0'; i = 0;
-			while ((dept[i++] = stringIn[k++]) != '\0');
-			cout << N << b << title << b << intcom << b << dept << endl;
-			fgets(stringIn, 99, fptrIn);
+			i = 0; j = 0;
+			while ((r.title[i++] = stringIn[j++]) != ',');
+			for (; i < 29; i++) r.title[i-1] = ' ';
+			r.title[i - 1] = '\0'; i = 0;
+			while ((r.intercomNum[i++] = stringIn[j++]) != ',');
+			r.intercomNum[i - 1] = '\0'; i = 0;
+			while ((r.dept[i++] = stringIn[j++]) != '\0');
+			cout << N << b << r.title << b << r.intercomNum << b << r.dept << endl;
+			collectStrings();
 			N++;
 		}
 		cout << endl << endl;
 		return;
 	}
-	};
-int main() {
-fileOps RdWrt;
-Department indiv;
+	// Function to print only 1 record
+};
 
+int main() {
+Record row;
+fileOps RdWrt;
+int options = 0;
 // Validation of success of file opening
 bool testing{};
 testing = RdWrt.validate();
-if (testing == true) {return -1;}
-
+if (testing == true){return -1;}
 // Look for a record
+// To print out all items in the database
 
+cout << "To display available records, enter '1': ";
+cin >> options;
+if (options == 1)
+	RdWrt.printAll(row);
 // Print out the result
-RdWrt.printAll();
-indiv.printNum();
-
-// To input new data
-
 
 return 0;
 }

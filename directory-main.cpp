cf1b0b54268edc62f1f75a7c4f4e9e4c2e039be1
BroVic <victorordu@outlook.com>
2016-07-13 17:01:25 +0100

    Add functionality for selecting records
    I made quite a few changes like altering the inheritance pattern and
    implementing polymorphism. At this stage, the program compiles well but
    still needs tweaking. Older code that worked well was commented out for
    the time being.

--- directory-main.cpp
+++ directory-main.cpp
@@ -24,116 +24,157 @@ class Record{
 };
 
 class fileOps:public Record{
-private:
-	FILE *fptrIn;
-	char stringIn[100];
-	char b;
-	int i, j, N;
-	string entry;
-public:
-	// constructors
-	fileOps() {
-		N = 1;
-		b = ' ';
-		string = "blank";
-		fptrIn = fopen("office-contact.csv", "r"); // review this line!!!
-		return;
-	}
-	~fileOps(){
-		fclose(fptrIn);
-		return;
-	}
-
-	bool validate(){
-	if (fptrIn == NULL){
-            cout << "Could not open database" << endl;
-            return true;
-            }
-    else {return false;}
-	}
-	
-	// Definition of some functions
-	// Function for collecting record strings
-	void collectStrings(){
-	fgets(stringIn, 99, fptrIn);
-	return;
-	}
-
-	// Function for splitting strings into parts
-	void splitString(){	
-			int i = 0; int j = 0;
-			while ((r.title[i++] = stringIn[j++]) != ',');
-			for (; i < 29; i++) r.title[i-1] = ' ';
-			r.title[i - 1] = '\0'; i = 0;
-			while ((r.intercomNum[i++] = stringIn[j++]) != ',');
-			r.intercomNum[i - 1] = '\0'; i = 0;
-			while ((r.dept[i++] = stringIn[j++]) != '\0');
+	protected:
+		FILE *fptrIn;
+		char stringIn[100];
+		char b;
+		int i, j, N;
+	public:
+		// constructors
+		fileOps() {
+			N = 1;
+			b = ' ';
+			stringIn[0] = {};
+			fptrIn = fopen("office-contact.csv", "r");
 			return;
-			}
-	
-	// Function to print all available records to the console
-	void printAll(Record &r){
-		collectStrings();
-		while(!feof(fptrIn)) {
-			splitString();
-			cout << N << b << r.title << b << r.intercomNum << b << r.dept << endl;
-			collectStrings();
-			N++;
 		}
-		cout << endl << endl;
-		return;
-	}
-	
-	// Function to print only selected records
-	void printSelect(string &input, char &y[], char &collector[]) {
-		cin >> x;
-		collectStrings();
-		// 5. Extract from the string an appropriate value
-		while(!feof(fptrIn)) {
-			splitString();
-			collector =  
-		// 6. Compare A & B		
-		// 7. If A = B, then store item of interest in C
-		// 8. If A != B, do nothing	
-		// 9. Repeat 4 - 9 until you reach end of the file
-			
-			cout << N << b << r.title << b << r.intercomNum << b << r.dept << endl;
-			collectStrings();
-			N++;
+		~fileOps(){
+			fclose(fptrIn);
+			return;
 		}
-	// 10. Print elements of C one after the other
-	}
 
+        // Pure virtual functions
+		virtual void printOut() = 0;   // to print output
+		virtual bool validate() = 0;   // to validate file opening
+
+		// Functions for file operations
+		void collectStrings(){          // for collecting strings from file
+		fgets(stringIn, 99, fptrIn);
+		return;
+		}
+
+		void splitString(){             // for splitting strings into parts
+				int i = 0; int j = 0;
+				while ((title[i++] = stringIn[j++]) != ',');
+				for (; i < 29; i++) title[i-1] = ' ';
+				title[i - 1] = '\0'; i = 0;
+				while ((intercomNum[i++] = stringIn[j++]) != ',');
+				intercomNum[i - 1] = '\0'; i = 0;
+				while ((dept[i++] = stringIn[j++]) != '\0');
+				return;
+				}
 };
 
-int main() {
-Record row;
-fileOps RdWrt;
-int options = 0;
-char trigger;
-// Validation of success of file opening
-bool testing{};
-testing = RdWrt.validate();
-if (testing == true){return -1;}
-// Look for a record
+class AllRecords:public fileOps {
+	public:
+		// Constructor/Deconstructor
+		AllRecords() {return;}
+		~AllRecords() {return;}
 
+		virtual bool validate(){
+		if (fptrIn == NULL){
+				cout << "Could not open to print EXISTING records" << endl;
+				return true;
+				}
+		else {return false;}
+		}
 
-// To print out all items in the database
-cout << "To display available records, enter '1': ";
-cin >> options;
-cout << "You selected " << options << "\n ";
-cout << "Do you want to continue {Y/N)?: ";
-cin >> trigger;
-if (trigger == 'Y' || trigger == 'y') {
-	if (options == 1){
-			RdWrt.printAll(row);
+		virtual void printOut(){            // for printing all existing records
+			collectStrings();
+			while(!feof(fptrIn)) {
+				splitString();
+				cout << N << b << title << b << intercomNum << b << dept << endl;
+				collectStrings();
+				N++;
 			}
-	}
-else if (trigger == 'N' || trigger == 'n'){
-		cout << "Thank you. Good bye" << endl;
-	}
-else {cout << trigger << " is not a valid entry. Exiting the application." << endl;}
-// Print out the result
+			cout << endl << endl;
+			return;
+		}
+};
 
+class SelectRecords:public fileOps {
+	private:
+		char str[];
+		char inTitle[];
+		int len;
+	public:
+		// Constructor/Deconstructor
+		SelectRecords() {
+		    str[0] = {};
+			inTitle[0] = {};
+			len = 0;
+			return;
+		}
+		~SelectRecords() {return;}
+
+		virtual bool validate(){
+		if (fptrIn == NULL){
+				cout << "Could not open to print SELECTED record(s)" << endl;
+				return true;
+				}
+		else {return false;}
+		}
+
+		virtual void printOut() {           // for printing selected records
+		    char tempIn[] = {}; char tempOut[] = {};
+            cout << "Enter Officer's title: ";
+            gets(str);
+            len = strlen(str);
+            for (int i = 0; i < len; i++) {
+                    inTitle[i] = str[i];
+            }
+            collectStrings();
+            while(!feof(fptrIn)) {
+                splitString();
+                for (i = 0;;i++){
+                    tempIn[i] = inTitle[i];
+                    tempOut[i] = title[i];
+                }
+                tempOut[j-1] = '\0';
+                if (tempIn == tempOut) {
+                    cout << N << b << title << b << intercomNum << b << dept << endl;
+                }
+                collectStrings();
+                N++;
+            }
+        }
+};
+
+int main() {// constructors
+	fileOps *RdWrt;
+	// AllRecords all;
+	SelectRecords some;
+	// char trigger;
+	// int options;
+	bool testing{};
+
+	// Validation of file opening
+
+
+	// Look for selected records
+	RdWrt = &some;
+	testing = RdWrt->validate();
+	if (testing == true){return -1;}
+	RdWrt->printOut();
+
+	/* To print out all items in the database
+	RdWrt = &all;
+	testing = RdWrt->validate();
+	if (testing == true){return -1;}
+	cout << "To display available records, enter '1': ";
+	cin >> options;
+	cout << "You selected " << options << "\n ";
+	cout << "Do you want to continue {Y/N)?: ";
+	cin >> trigger;
+	if (trigger == 'Y' || trigger == 'y') {
+		if (options == 1){
+				RdWrt->printOut();
+		}
+	}
+	else if (trigger == 'N' || trigger == 'n'){
+			cout << "Thank you. Good bye" << endl;
+	}
+	else {cout << trigger << " is not a valid entry. Exiting the application." << endl;}
+	*/
 return 0;
 }

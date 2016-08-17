f6d4ff305a594958aad383dffdb0d57dd9bea476
BroVic <victorordu@outlook.com>
2016-08-17 09:21:03 +0100

    Continues process of modifying classes
    I am using the string class in the program and have started editing the
    code to that effect. Progran not yet functional.

--- Directory_vs/Directory_vs/SelectRecords.h
+++ Directory_vs/Directory_vs/SelectRecords.h
@@ -2,51 +2,56 @@
 #define SELECTRECORDS_H_INCLUDED
 class SelectRecords :public fileOps {
 private:
-	char typed_string[30], tempStr[30];
+	std::string typed_string, tempStr;
 	signed int comparison;
 	int SN;
 	size_t t;
 public:
 	// Constructor/Deconstructor
 	SelectRecords() {
-		typed_string[0] = {}; tempStr[0] = {};
+		typed_string;
+		tempStr;
 		comparison = -1;
 		SN = 1;
 		t = 0;
 	}
 	~SelectRecords() { };
 
-	virtual bool validate() {
-		if (fptrIn == NULL) {
-			std::cout << "Could not open to print SELECTED record(s)" << std::endl;
-			return true;
-		}
-		else { return false; }
-	}
-
-	virtual void printOut() {
-		// for printing selected records
-		printf("Enter Officer's title: ");
-		gets_s(typed_string, 29);
-		// get the length of the entered string + NULL character
-
-		collectStrings();
-		while (!feof(fptrIn)) {
-			// extract a string of equivalent length from the string on file
-			for (t = 0; t < strlen(typed_string); t++) {
-				tempStr[t] = file_string[t];
-			}
-			tempStr[t] = '\0';
-			// compare both strings
-			comparison = strcmp(tempStr, typed_string);
-			// if identical, print out the string from file
-			// else do nothing
-			if (comparison == 0) {
-				printf("%s \n", file_string);
-			}
-			collectStrings();
-			SN++;
-		}
-	}
+	virtual bool validate();  // to check whether memmore was allocated successfully
+	virtual void printOut();  // to print out the selected records
 };
 #endif // SELECTRECORDS_H_INCLUDED
+
+// Function definitions
+bool SelectRecords::validate() {
+	if (fptrIn == nullptr) {
+		std::cout << "Could not open to print SELECTED record(s)" << std::endl;
+		return true;
+	}
+	else { return false; }
+}
+
+void SelectRecords::printOut() {
+	// for printing selected records
+	printf("Enter Officer's title: ");
+	gets_s(typed_string, 29);
+	// get the length of the entered string + NULL character
+
+	collectStrings();
+	while (!feof(fptrIn)) {
+		// extract a string of equivalent length from the string on file
+		for (t = 0; t < strlen(typed_string); t++) {
+			tempStr[t] = file_string[t];
+		}
+		tempStr[t] = '\0';
+		// compare both strings
+		comparison = strcmp(tempStr, typed_string);
+		// if identical, print out the string from file
+		// else do nothing
+		if (comparison == 0) {
+			printf("%s \n", file_string);
+		}
+		collectStrings();
+		SN++;
+	}
+}
--- Directory_vs/Directory_vs/fileOps.h
+++ Directory_vs/Directory_vs/fileOps.h
@@ -1,38 +1,58 @@
 #ifndef FILEOPS_H_INCLUDED
 #define FILEOPS_H_INCLUDED
-class fileOps :public Record {
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+class fileOps :public Record
+{
 private:
 	int i, j;  // counters
 protected:
-	FILE *fptrIn;
-	char file_string[95];  // to be changed to pointer instead
+	ifstream fromFile;
+	string file_string;
 public:
-	fileOps() {
-		fopen_s(&fptrIn, "office-contact.csv", "r");
-		file_string[0] = {};
-		i = 0; j = 0;
-	}
-	~fileOps() { fclose(fptrIn); }
+	fileOps() { }
+	~fileOps() { fromFile.close(); }
 
-	// Pure virtual functions to be implemented in derived classes
 	virtual void printOut() = 0;   // to print output
 	virtual bool validate() = 0;   // to validate file opening
 
-								   // Functions for file operations
-	void collectStrings() {          // for collecting strings from file
-		fgets(file_string, 99, fptrIn);
-		return;
-	}
-
-	void splitString() {             // for splitting strings into parts
-		i = 0; j = 0;
-		while ((title[i++] = file_string[j++]) != ',');
-		for (; i < 29; i++) title[i - 1] = ' ';
-		title[i - 1] = '\0'; i = 0;
-		while ((intercomNum[i++] = file_string[j++]) != ',');
-		intercomNum[i - 1] = '\0'; i = 0;
-		while ((dept[i++] = file_string[j++]) != '\0');
-		return;
-	}
+	// File operations
+	void collectStrings();          // for collecting strings from file
+	void splitString();             // for splitting strings into parts
+		
 };
 #endif // FILEOPS_H_INCLUDED
+
+fileOps::fileOps()
+{
+	fromFile("office-contact.csv");
+	file_string;
+}
+
+void fileOps::collectStrings() 
+{ 
+	if (fromFile.is_open)
+	{
+		while (getline(fromFile, file_string))
+		{
+
+		}
+	}
+	
+	return;
+}
+
+void fileOps::splitString() 
+{
+	i = 0; j = 0;
+	while ((title[i++] = file_string[j++]) != ',');
+	for (; i < 29; i++) title[i - 1] = ' ';
+	title[i - 1] = '\0'; i = 0;
+	while ((intercomNum[i++] = file_string[j++]) != ',');
+	intercomNum[i - 1] = '\0'; i = 0;
+	while ((dept[i++] = file_string[j++]) != '\0');
+	return;
+}
